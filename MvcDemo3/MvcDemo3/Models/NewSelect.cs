8595a24c3dd05c3105d7f2df9d4fbbb2fb766694
e <489347378@qq.com>
2016-12-27 16:56:15 +0800

    MVC4Demo

---
+++ MvcDemo3/MvcDemo3/Models/NewSelect.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemo3.Models
+{
+    public class NewSelect
+    {
+        //public string ID { get; set; }
+        public string A_ProcessID { get; set; }
+        //public string K2ProcessInstID { get; set; }
+        public string A_StaffCode { get; set; }
+        public string A_StaffName { get; set; }
+        //public string HolidayType { get; set; }
+        //public Nullable<System.DateTime> StratDate { get; set; }
+        //public Nullable<System.DateTime> EndDate { get; set; }
+        //public string StartTimeSpan { get; set; }
+        //public string EndTimeSpan { get; set; }
+        //public Nullable<decimal> CalendarDays { get; set; }
+        //public Nullable<decimal> WorkingDays { get; set; }
+        //public string ReasonForLeave { get; set; }
+        //public Nullable<System.DateTime> CreateDate { get; set; }
+        //public Nullable<System.DateTime> ModifiedDate { get; set; }
+        //public string StaffAccount { get; set; }
+        //public string DelegateStaffAccount { get; set; }
+
+        //public string ID { get; set; }
+        public string ELID { get; set; }
+        public string Field1 { get; set; }
+        public string Field2 { get; set; }
+        //public string Field3 { get; set; }
+        //public string Field4 { get; set; }
+        //public string Field5 { get; set; }
+        //public string Field6 { get; set; }
+        //public string Field7 { get; set; }
+        //public string Field8 { get; set; }
+        //public string Field9 { get; set; }
+        //public string Field10 { get; set; }
+        //public string Field11 { get; set; }
+        //public string Field12 { get; set; }
+    }
+}
